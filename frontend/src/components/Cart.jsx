b794c07a84b0de69cd47093a2fb4f64381ceaba1
harshivg <ganwaniharshiv@gmail.com>
2024-08-15 15:25:23 +0530

    checkout

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 export const Cart = ({ cart, total, scrollToBottom }) => {
   const discount = 5;
   const [showCart, setshowCart] = useState(false);
+  
   const handleShowCart = () => {
     setshowCart(!showCart);
   }

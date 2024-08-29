13bb1377bef8a5f9c9dc21535bff39f640325923
Harsh Tayal <harshtayal2005@gmail.com>
2024-08-29 19:08:09 +0530

    deployed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mainRouter = require("./routes/index");
 const cors = require("cors");
 const Hello = require("./api/hello");
+require('dotenv').config();
+
 
 const app = express();
 app.use(express.json());

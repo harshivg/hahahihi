457902491989f412d25aa747babea012cdf13fc0
Harsh Tayal <harshtayal2005@gmail.com>
2024-08-29 12:24:22 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mainRouter = require("./routes/index");
 const cors = require("cors");
+const Hello = require("./api/hello");
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(cors());
 
 app.use("/api/v1", mainRouter);
 
+app.get("/", Hello);
 app.listen(3000,  () => {
     console.log("Server is running on port 3000");
 });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ const userRouter = require("./user");
 const accountRouter = require("./account");
 const itemRouter = require("./item");
 
+
 const router = express.Router();
 router.use("/user", userRouter);
 router.use("/account", accountRouter);

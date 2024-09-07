c9e6a3f2c738327acbf12798886fa95804400362
Himanshu syuni <124446173+himanshusyuni@users.noreply.github.com>
2024-09-07 11:24:26 +0530

    Update Items.jsx

--- frontend/src/components/Items.jsx
+++ frontend/src/components/Items.jsx
@@ -29,7 +29,7 @@ export const Items = ({ fetchCart }) => {
         })
             .then((response) => {
               console.log(response);
-                setItems(response.data.items);
+                setItems(response);
             });
     }, [debouncedFilter]);
 

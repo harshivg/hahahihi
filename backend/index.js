084c83c4bbff7e46e0a1f5034f0ed01676724a68
Himanshu syuni <124446173+himanshusyuni@users.noreply.github.com>
2024-09-07 09:59:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use('/', (req, res) => {
-//   res.status(200).json({ message: "Hello from Vercel!" });
-// })
-app.use("/", mainRouter);
+app.use('/', (req, res) => {
+  res.status(200).json({ message: "Hello from Vercel!" });
+})
+app.use("/api/", mainRouter);
 
 app.listen(port,  () => {
     console.log("Server is running on port " + port);

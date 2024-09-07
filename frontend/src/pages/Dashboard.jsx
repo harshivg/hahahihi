5e1e3687482c4899c221bb2ff2d64f515f7953fa
Himanshu syuni <124446173+himanshusyuni@users.noreply.github.com>
2024-09-07 10:01:23 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -28,7 +28,7 @@ export const Dashboard = () => {
   let longSide = screenWidth / 2;
   const fetchCart = () => {
     axios
-      .get(`https://shoppy-mart-ochre.vercel.app/item/cart`, {
+      .get(`https://shoppy-mart-ochre.vercel.app/api/item/cart`, {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("token"),
         },
